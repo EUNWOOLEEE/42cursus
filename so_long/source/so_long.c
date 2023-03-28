1bd497ab38f3a4752ebb20034784e1e812923a03
eunwoo <lkjin0110@gmail.com>
2023-03-28 21:22:16 +0900

    .

--- so_long/source/so_long.c
+++ so_long/source/so_long.c
@@ -6,16 +6,18 @@
 /*   By: eunwolee <eunwolee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 14:22:15 by eunwolee          #+#    #+#             */
-/*   Updated: 2023/03/28 19:18:51 by eunwolee         ###   ########.fr       */
+/*   Updated: 2023/03/28 20:20:10 by eunwolee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
 //x버튼으로 종료되게 하기
-//exit로 종료되게 하기
-//맨대토리에서 움직임 없게 하기
+//exit()로 종료되게 하기
+//같은 내용으로 bonus 파일 만들기. mandatory 미허용 함수 없는지 확인
 //mlx 함수들 리턴값 확인하기
+//검은 십자 선 없애기
+//segfalut, bus error 해결하기
 
 int	main(int argc, char **argv)
 {
--- so_long/source/stand.c
+++ so_long/source/stand.c
@@ -6,7 +6,7 @@
 /*   By: eunwolee <eunwolee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 16:20:20 by eunwolee          #+#    #+#             */
-/*   Updated: 2023/03/28 19:37:53 by eunwolee         ###   ########.fr       */
+/*   Updated: 2023/03/28 20:45:53 by eunwolee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,22 +28,9 @@ void	standing(t_game *game)
 	}
 }
 
-static void	draw_motion(t_game *game)
-{
-	draw_img(game, game->map_img[0], game->cur.row * 32, game->cur.col * 32);
-	if (game->flag.rest)
-		draw_img(game, game->rest[game->cur_dir][game->frame / 8],
-			game->cur.row * 32, game->cur.col * 32);
-	else if (game->flag.stand)
-		draw_img(game, game->stand[game->cur_dir][game->frame / 8],
-			game->cur.row * 32, game->cur.col * 32);
-	else if (game->flag.sleep)
-		draw_img(game, game->sleep[game->cur_dir][game->frame / 8],
-			game->cur.row * 32, game->cur.col * 32);
-}
-
 static void	set_flag(t_game *game)
 {
+	printf("a\n");
 	game->flag.rest = 0;
 	game->flag.stand = 0;
 	game->flag.sleep = 0;
@@ -61,8 +48,24 @@ static void	set_flag(t_game *game)
 	}
 }
 
+static void	draw_motion(t_game *game)
+{
+	printf("b\n");
+	draw_img(game, game->map_img[0], game->cur.row * 32, game->cur.col * 32);
+	if (game->flag.rest)
+		draw_img(game, game->rest[game->cur_dir][game->frame / 8],
+			game->cur.row * 32, game->cur.col * 32);
+	else if (game->flag.stand)
+		draw_img(game, game->stand[game->cur_dir][game->frame / 8],
+			game->cur.row * 32, game->cur.col * 32);
+	else if (game->flag.sleep)
+		draw_img(game, game->sleep[game->cur_dir][game->frame / 8],
+			game->cur.row * 32, game->cur.col * 32);
+}
+
 static int	check_reset_point(t_game *game)
 {
+	printf("c\n");
 	if (game->flag.rest && game->frame == 112)
 		return (1);
 	if (game->flag.stand && game->frame == 40)
