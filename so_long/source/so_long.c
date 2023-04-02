97e830dbdb88cd840de5032324dec332f9afcff0
eunwoo <lkjin0110@gmail.com>
2023-04-02 13:46:53 +0900

    .

--- so_long/source/so_long.c
+++ so_long/source/so_long.c
@@ -6,13 +6,12 @@
 /*   By: eunwolee <eunwolee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 14:22:15 by eunwolee          #+#    #+#             */
-/*   Updated: 2023/04/01 13:15:30 by eunwolee         ###   ########.fr       */
+/*   Updated: 2023/04/01 14:27:19 by eunwolee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-//시작 시에 sleep 상태, seg fault 해결하기
 //같은 내용으로 bonus 파일 만들고 makefile rule 추가하기
 
 int	main(int argc, char **argv)
