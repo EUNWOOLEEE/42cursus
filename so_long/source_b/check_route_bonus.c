e8520708d0a7c2ff7344200766f3e5de00592b4c
eunwoo <lkjin0110@gmail.com>
2023-04-04 20:24:36 +0900

    .

--- so_long/source_b/check_route_bonus.c
+++ so_long/source_b/check_route_bonus.c
@@ -6,7 +6,7 @@
 /*   By: eunwolee <eunwolee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 20:10:04 by eunwolee          #+#    #+#             */
-/*   Updated: 2023/04/04 19:52:20 by eunwolee         ###   ########.fr       */
+/*   Updated: 2023/04/04 20:23:45 by eunwolee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int			check_route(t_game *game, int flag);
 static void	bfs(t_game *game, t_coor *queue, int front, int rear);
-static int	*set_direction(int flag);
+static void	set_direction(int **d_row, int **d_col);
 static int	apply_direction(t_game *game, int next_row, int next_col);
 static int	check_res(t_game *game, int flag);
 
@@ -32,6 +32,9 @@ int	check_route(t_game *game, int flag)
 	return (check_res(game, flag));
 }
 
+
+#include <stdio.h>
+
 static void	bfs(t_game *game, t_coor *queue, int front, int rear)
 {
 	int		cnt;
@@ -40,8 +43,7 @@ static void	bfs(t_game *game, t_coor *queue, int front, int rear)
 	int		*d_row;
 	int		*d_col;
 
-	d_row = set_direction(1); //한번에 초기화하기
-	d_col = set_direction(2);
+	set_direction(&d_row, &d_col);
 	queue[0].row = game->map.start[0];
 	queue[0].col = game->map.start[1];
 	game->map.tmp[game->map.start[0]][game->map.start[1]] = '-';
@@ -62,28 +64,20 @@ static void	bfs(t_game *game, t_coor *queue, int front, int rear)
 	free(d_col);
 }
 
-static int	*set_direction(int flag)
+static void	set_direction(int **d_row, int **d_col)
 {
-	int	*tmp;
-
-	tmp = (int *)ft_calloc(4, sizeof(int));
-	if (!tmp)
+	*d_row = (int *)ft_calloc(4, sizeof(int));
+	*d_col = (int *)ft_calloc(4, sizeof(int));
+	if (!*d_row || !*d_col)
 		error_exit(0);
-	if (flag == 1)
-	{
-		tmp[0] = -1;
-		tmp[1] = 1;
-		tmp[2] = 0;
-		tmp[3] = 0;
-	}
-	else if (flag == 2)
-	{
-		tmp[0] = 0;
-		tmp[1] = 0;
-		tmp[2] = -1;
-		tmp[3] = 1;
-	}
-	return (tmp);
+	(*d_row)[0] = -1;
+	(*d_row)[1] = 1;
+	(*d_row)[2] = 0;
+	(*d_row)[3] = 0;
+	(*d_col)[0] = 0;
+	(*d_col)[1] = 0;
+	(*d_col)[2] = -1;
+	(*d_col)[3] = 1;
 }
 
 static int	apply_direction(t_game *game, int next_row, int next_col)
