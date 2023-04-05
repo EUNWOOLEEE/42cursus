9a309ffe6d554872a5fbf630540bfb04e164a6b2
eunwoo <lkjin0110@gmail.com>
2023-04-05 14:00:55 +0900

    .

--- so_long/Libft/source/ft_strjoin.c
+++ so_long/Libft/source/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: eunwolee <eunwolee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 20:57:36 by eunwolee          #+#    #+#             */
-/*   Updated: 2023/04/04 20:41:27 by eunwolee         ###   ########.fr       */
+/*   Updated: 2023/04/05 13:51:05 by eunwolee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ char	*ft_strjoin(char *s1, char *s2)
 {
 	int		idx;
 	char	*str;
-	char	*tmp;
 
 	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!str)
@@ -24,7 +23,6 @@ char	*ft_strjoin(char *s1, char *s2)
 		return (0);
 	}
 	idx = 0;
-	tmp = s1;
 	while (*s1)
 		str[idx++] = *s1++;
 	while (*s2)
