9a73579a66d24d3a0a565c66b1eb29b05bd6341e
eunwoo <lkjin0110@gmail.com>
2023-07-04 07:49:29 +0900

    hash table 1

--- minishell/incs/hash.h
+++ minishell/incs/hash.h
@@ -6,7 +6,7 @@
 /*   By: eunwolee <eunwolee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 19:07:26 by eunwolee          #+#    #+#             */
-/*   Updated: 2023/07/03 19:25:36 by eunwolee         ###   ########.fr       */
+/*   Updated: 2023/07/04 07:46:21 by eunwolee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ typedef struct s_node
 {
 	int				key;
 	char			*value;
-	struct s_node	*hash;
+	struct s_node	*next;
 }	t_node;
 
 //배열로 만들어서 사용
@@ -26,5 +26,14 @@ typedef struct s_bucket
 	t_node	*root;
 	int		size;
 }	t_bucket;
+
+/*hash_table*/
+int		h_get_idx(int table_size, int key);
+bool	h_add(t_bucket *table, int table_size, char *value);
+char	*h_search(t_bucket *table, int table_size, int key);
+
+/*hash_remove*/
+bool	h_remove_one(t_bucket *table, int table_size, int key);
+void	h_clear_all(t_bucket **table, int table_size);
 
 #endif
