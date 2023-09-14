27844dda0860ffd6bb1f9c99be39953c25694e80
eunwoo <lkjin0110@gmail.com>
2023-09-14 16:17:46 +0900

    .

--- CPP/Module00/ex01/srcs/Contact.cpp
+++ CPP/Module00/ex01/srcs/Contact.cpp
@@ -6,13 +6,14 @@
 /*   By: eunwolee <eunwolee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 21:02:17 by eunwolee          #+#    #+#             */
-/*   Updated: 2023/09/14 15:56:46 by eunwolee         ###   ########.fr       */
+/*   Updated: 2023/09/14 16:17:05 by eunwolee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/Phonebook.hpp"
 
 static void get_line(std::string *str, std::string *arr);
+static bool	check_space(std::string str);
 
 std::string Contact::get_info(int i){
 	return info_arr[i];
@@ -37,11 +38,17 @@ void	Contact::add_info(void){
 }
 
 static void get_line(std::string *str, std::string *arr){
-	while(str->empty() == true || std::all_of(str->begin(), str->end(), isspace) == true)
-	{
+	while(str->empty() == true || check_space(*str) == true){
 		std::getline(std::cin, *str);
 		if (std::cin.eof() == true)
 			std::exit(0);
 	}
 	*arr = *str;
+}
+
+static bool	check_space(std::string str){
+	for(unsigned long i = 0; i < str.size(); i++)
+		if(isspace(str[i]) == false)
+			return false;
+	return true;
 }
