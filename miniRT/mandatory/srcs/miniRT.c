9026002d446bdb080b2d4a7218d7e5ca7a73d710
eunwoo <lkjin0110@gmail.com>
2023-10-31 15:55:31 +0900

    .

--- miniRT/mandatory/srcs/miniRT.c
+++ miniRT/mandatory/srcs/miniRT.c
@@ -6,22 +6,16 @@
 /*   By: eunwolee <eunwolee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 22:07:02 by eunwolee          #+#    #+#             */
-/*   Updated: 2023/10/31 15:38:25 by eunwolee         ###   ########.fr       */
+/*   Updated: 2023/10/31 15:54:10 by eunwolee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/miniRT.h"
 
-void	asd()
-{
-	system("leaks miniRT");
-}
-
 int	main(int argc, char **argv)
 {
 	t_scene	*scene;
 
-	// atexit(asd);
 	scene = scene_init(argc, argv[1]);
 	scene_read(scene);
 	img_set(scene);
