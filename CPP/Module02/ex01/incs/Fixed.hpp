be0d7b6b08e736d7917fc8b504fb605c390bb2e1
eunwoo <lkjin0110@gmail.com>
2023-11-01 12:24:52 +0900

    .

--- CPP/Module02/ex01/incs/Fixed.hpp
+++ CPP/Module02/ex01/incs/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: eunwolee <eunwolee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 19:58:22 by eunwolee          #+#    #+#             */
-/*   Updated: 2023/10/31 20:11:44 by eunwolee         ###   ########.fr       */
+/*   Updated: 2023/10/31 20:09:21 by eunwolee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,22 @@ class Fixed{
 
 public:
 	Fixed();
+	Fixed(int n);
+	Fixed(float n);
 	Fixed(const Fixed& obj);
 	~Fixed();
 
 	Fixed& operator=(const Fixed& src);
+	// Fixed& operator<<(const Fixed& src); //출력 스트림 객체로 전달된 부동 소수점 표현을 삽입
 
-	int getRawBits( void ) const;
-	void setRawBits( int const raw );
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat(void) const;
+	int toInt(void) const;
 
 private:
-	int					fix;
+	int					integerPart;
+	int					fractionalPart;
 	static const int	bit;
 
 };
--- CPP/Module02/ex01/srcs/Fixed.cpp
+++ CPP/Module02/ex01/srcs/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: eunwolee <eunwolee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 20:03:59 by eunwolee          #+#    #+#             */
-/*   Updated: 2023/10/31 16:59:42 by eunwolee         ###   ########.fr       */
+/*   Updated: 2023/10/31 20:09:38 by eunwolee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,18 @@ const int Fixed::bit = 8;
 
 Fixed::Fixed(){
 	std::cout << "Default constructor called\n";
-	fix = 0;
+	integerPart = 0;
+	fractionalPart = 0;
+}
+
+Fixed::Fixed(int n){
+	std::cout << "Int constructor called\n";
+	fix = n; //고정 소수점 값으로 변환
+}
+
+Fixed::Fixed(float n){
+	std::cout << "Float constructor called\n";
+	fix = n; //고정 소수점 값으로 변환
 }
 
 Fixed::Fixed(const Fixed& obj){
@@ -42,4 +53,12 @@ int Fixed::getRawBits(void) const{
 
 void Fixed::setRawBits(int const raw){
 	fix = raw;
+}
+
+float Fixed::toFloat(void) const{
+	// fixed-point 값을 부동 소수점 값으로 변환하는 멤버 함수
+}
+
+int Fixed::toInt(void) const{
+	// fixed-point 값을 정수 값으로 변환하는 멤버 함수
 }
