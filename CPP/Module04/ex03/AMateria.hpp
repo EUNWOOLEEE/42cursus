fa7b90f4278d111d5fa73a8956dbda2aa0b2d69e
eunwoo <lkjin0110@gmail.com>
2023-11-16 19:19:03 +0900

    .

---
+++ CPP/Module04/ex03/AMateria.hpp
@@ -0,0 +1,16 @@
+class AMateria {
+
+protected:
+        // [...]
+
+public:
+	AMateria(std::string const & type);
+	// [...]
+
+	std::string const & getType() const; //Returns the materia type
+	
+	virtual AMateria* clone() const = 0;
+	virtual void use(ICharacter& target);
+};
+
+virtual void use(ICharacter& target);
