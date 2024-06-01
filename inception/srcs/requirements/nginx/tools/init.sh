3c6434ef7adafcde7b4142c463226bec3ebef796
eunwoo <lkjin0110@gmail.com>
2024-06-01 18:48:07 +0900

    .

--- inception/srcs/requirements/nginx/tools/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-openssl	req -x509 -nodes -days 365 -newkey rsa:2048							\
-		-keyout /etc/ssl/private/nginx-selfsigned.key						\
-		-out /etc/ssl/certs/nginx-selfsigned.crt							\
-		-subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=42seoul/CN=$DOMAIN_NAME"
-
-exec nginx -g "daemon off;"
